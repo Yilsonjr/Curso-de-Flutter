c2d1ccd9a2c195d50bbe07e22bf6f685743d5cef
Yilsonjr <ing.jimrod@gmail.com>
2020-05-10 18:27:41 -0400

    Practica curso flutter -Stepper

---
+++ stepper_widget/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+   home: MyStepper(),
+  ));
+}
+
+class Mystepper extends StatefulWidget {
+  Mystepper({Key key}) : super(key: key);
+
+  @override
+  _MystepperState createState() => _MystepperState();
+}
+
+class _MystepperState extends State<Mystepper> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
