b81edd4bae0cb30862af2589a1145e955d026093
Yilsonjr <ing.jimrod@gmail.com>
2020-05-11 21:44:35 -0400

    Update main.dart

--- stepper_widget/lib/main.dart
+++ stepper_widget/lib/main.dart
@@ -5,19 +5,66 @@ void main() {
    home: MyStepper(),
   ));
 }
-
-class Mystepper extends StatefulWidget {
-  Mystepper({Key key}) : super(key: key);
-
+class MyStepper extends StatefulWidget {
   @override
-  _MystepperState createState() => _MystepperState();
+  _MyStepperState createState() => _MyStepperState();
 }
 
-class _MystepperState extends State<Mystepper> {
+class _MyStepperState extends State<MyStepper> {
+  int _currentStep = 0;
+  List<Step> mySteps = [
+    new Step(
+        title: new Text("Paso 1", style: TextStyle(fontSize: 26),),
+        content: new Text("Aprender flutter", style: TextStyle(fontSize: 26,color: Colors.deepOrange,fontWeight: FontWeight.bold)),
+    ),
+    new Step(
+      title: new Text("Paso 2", style: TextStyle(fontSize: 26),),
+      content: new Text("Desarrollar App", style: TextStyle(fontSize: 26,color: Colors.deepOrange,fontWeight: FontWeight.bold)),
+    ),
+    new Step(
+      title: new Text("Paso 3", style: TextStyle(fontSize: 26),),
+      content: new Text("Publicar Aplicacion", style: TextStyle(fontSize: 26,color: Colors.deepOrange,fontWeight: FontWeight.bold)),
+    ),
+    new Step(
+      title: new Text("Paso 4", style: TextStyle(fontSize: 26),),
+      content: new Text("Ganar Dinero Manitooo..", style: TextStyle(fontSize: 26,color: Colors.deepOrange,fontWeight: FontWeight.bold)),
+    )
+  ];
   @override
+
   Widget build(BuildContext context) {
-    return Container(
-       child: child,
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Utilizando Stepper")
+      ),
+      body: new Container(
+          child: new Stepper(
+            steps: mySteps,
+            currentStep:this._currentStep , // Nos indica cual es el estado actual del Stepper
+            onStepContinue: (){
+              setState(() {
+
+                if(_currentStep < mySteps.length -1) { _currentStep++;}
+                else{_currentStep = 0;}
+              });
+            },
+            onStepTapped: (step){
+              setState(() {
+                _currentStep = step;
+              });
+            },
+            onStepCancel: (){
+              setState(() {
+                if(_currentStep > 0){
+                  _currentStep = _currentStep -1;
+                }else{
+                  _currentStep = 0;
+                }
+
+              });
+            },
+          ),
+      ),
     );
   }
 }
